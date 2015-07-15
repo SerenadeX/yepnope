a2e5d32ffb1c899c9e080b2e0017b052a34b80a1
Rhett Rogers <serenade.xs@icloud.com>
2015-07-15 14:03:33 -0600

    Added some testing files

--- test/file1.js
+++ test/file1.js
@@ -1,10 +1,12 @@
 window.file1 = true;
-yepnope({
-  load : 'file2.js',
-  callback: function() {
-    ok( window.file2, 'file2.js has loaded.');
-  },
-  complete: function() {
-  	start();
-  }
-});
+console.log("file1 loaded");
+
+// yepnope({
+//   load : 'file2.js',
+//   callback: function() {
+//     // ok( window.file2, 'file2.js has loaded.');
+//   },
+//   complete: function() {
+//   	// start();
+//   }
+// });
